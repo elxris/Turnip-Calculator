fc082e6b46425d28a058c4743d8a7787dc334ff5
Christian C <cristiancehi@gmail.com>
2020-04-07 20:50:06 -0500

    Guaranteed Min. New Changelog.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { Box, Typography, Link } from "@material-ui/core";
-
-import i18n from "./i18n";
+import { Box, Typography, Link as MaterialLink } from "@material-ui/core";
 import { Trans } from "react-i18next";
+import { string, node } from "prop-types";
+import i18n from "./i18n";
+
+const Link = ({ href, gh, children }) => (
+  <MaterialLink
+    href={href || `https://github.com/elxris/Turnip-Calculator/${gh}`}
+    target="_blank"
+    rel="noopener"
+  >
+    {children}
+  </MaterialLink>
+);
+Link.propTypes = {
+  href: string,
+  gh: string,
+  children: node.required,
+};
+Link.defaults = {
+  href: undefined,
+  gh: undefined,
+};
 
 const Footer = () => {
   return (
@@ -21,86 +40,68 @@ const Footer = () => {
             - The <b>Buy Price</b> value is for your own island. It doesn&#39;t
             matter if you buy it in other island!
           </Trans>
-          <br/>
+          <br />
           <Trans i18nKey="priceChangeInfo">
-            - Prices change <b>twice a day</b>. Be sure to log them.
-            (We save your data in your device).
+            - Prices change <b>twice a day</b>. Be sure to log them. (We save
+            your data in your device).
+          </Trans>
+          <br />
+          <Trans i18nKey="guaranteedMinInfo">
+            - The <b>Week min</b> value is a guaranteed minimum price you will
+            get this week. Wait at least for this price.
           </Trans>
         </Typography>
       </Box>
       <Box my={2}>
-        <Typography variant="h5">{i18n.t('Changelog')}</Typography>
+        <Typography variant="h5">{i18n.t("Changelog")}</Typography>
         <Typography variant="body1">
-          {i18n.t('changelog1.1')} <br />
-          {i18n.t('changelog1.2a')}
+          {i18n.t("changelog1.1")} <br />
+          {i18n.t("changelog1.2a")}
           <br />
           <Trans i18nKey="changelog1.3">
-            1.3: Animations fixed. New icon, thanks {" "}
-              <Link
-                href="https://github.com/elxris/Turnip-Calculator/pull/2"
-                target="_blank"
-                rel="noopener"
-              >
-                @mtaylor76
-              </Link>
-              ! New horizontal marker for buy price, thanks{" "}
-              <Link
-                href="https://github.com/elxris/Turnip-Calculator/issues/5"
-                target="_blank"
-                rel="noopener"
-              >
-                @pudquick
-              </Link>
-              !
+            1.3: Animations fixed. New icon (<Link gh="pull/2">@mtaylor76</Link>
+            ). New horizontal marker for buy price (
+            <Link gh="issues/5">@pudquick</Link>
+            )!
+          </Trans>
+          <br />
+          <Trans i18nKey="changelog1.4">
+            1.4: Guaranteed Min. (
+            <Link href="pull/10">@capoferro &amp; @nanoNago</Link>
+            ). Localisation Support (<Link gh="pull/15">@mtaylor76</Link>).
+            French Localisation (<Link gh="pull/16">@Ekaekale</Link>). New
+            Currency Icon (<Link gh="pull/11">@alyphen</Link>). Bug Reporters (
+            <Link gh="issues/14">@FoxFireX</Link>).
           </Trans>
         </Typography>
       </Box>
       <Box my={2}>
-        <Typography variant="h5">{i18n.t('About')}</Typography>
+        <Typography variant="h5">{i18n.t("About")}</Typography>
         <Typography variant="body1">
           <Trans i18nKey="about1">
             This wouldn&apos;t be possible without{" "}
-            <Link
-              href="https://twitter.com/_Ninji/status/1244818665851289602"
-              target="_blank"
-              rel="noopener"
-            >
+            <Link href="https://twitter.com/_Ninji/status/1244818665851289602">
               @_Ninji&apos;s
             </Link>{" "}
-            effort. 
+            effort.
           </Trans>
           <br />
           <Trans i18nKey="about2">
             I got this inspiration from{" "}
-            <Link
-              href="https://mikebryant.github.io/ac-nh-turnip-prices/index.html"
-              target="_blank"
-              rel="noopener"
-            >
+            <Link href="https://mikebryant.github.io/ac-nh-turnip-prices/index.html">
               mikebryant&apos;s
             </Link>{" "}
-            work. 
+            work.
           </Trans>
           <br />
           <Trans i18nKey="about3">
-             He is game dev! Follow him on twitter:{" "}
-            <Link
-              href="https://twitter.com/Consalv0"
-              target="_blank"
-              rel="noopener"
-            >
-              @Consalv0
-            </Link>
-            .
+            He is game dev! Follow him on twitter:{" "}
+            <Link href="https://twitter.com/Consalv0">@Consalv0</Link>.
           </Trans>
           <br />
           <Trans i18nKey="about4">
             A bug? Report it{" "}
-            <Link
-              href="https://github.com/elxris/Turnip-Calculator/issues"
-              target="_blank"
-              rel="noopener"
-            >
+            <Link href="https://github.com/elxris/Turnip-Calculator/issues">
               here
             </Link>
             .
