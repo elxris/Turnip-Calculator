832fa54b980ef06c947cee5aad2ae688db080bbd
Christian Ceciliano <cristiancehi@gmail.com>
2020-04-09 11:11:57 -0500

    Update src/Localizer.jsx

--- src/Localizer.jsx
+++ src/Localizer.jsx
@@ -14,7 +14,7 @@ const languages = [
   ["zh-HK", "繁體中文（香港）"],
   ["ko", "한국어"],
   ["ja", "日本語"],
-  ["pt-br", "Português"],
+  ["pt-BR", "Português"],
 ];
 
 const useStyles = makeStyles((theme) => ({
