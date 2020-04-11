fc98ed069e5c2c8a959e8b5f82e0c58f4e4c8a1c
Christian C <cristiancehi@gmail.com>
2020-04-11 13:54:50 -0500

    Updates

---
+++ precache-manifest.7fc388944e8b49d196732f50ffcdbe92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e6655430cf99f62344d9b437d9868d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abbf7dcf17f9683c8e05",
+    "url": "./static/js/2.34857d59.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "./static/js/2.34857d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff99a1f4395affdf0481",
+    "url": "./static/js/main.1ea465d5.chunk.js"
+  },
+  {
+    "revision": "2dacdc7c1ae4fe6ccd1d",
+    "url": "./static/js/runtime-main.1e8c8d06.js"
+  },
+  {
+    "revision": "d5f521c10f03b7f0f1f98411bd3f9a99",
+    "url": "./static/media/bells.d5f521c1.svg"
+  }
+]);
