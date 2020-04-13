816879db2d5b6ba04dca073720ceafde5d482411
Christian C <cristiancehi@gmail.com>
2020-04-13 09:59:56 -0500

    Version bump 1.8-beta

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -115,7 +115,7 @@ const Footer = () => {
         </Box>
         <Localizer />
         <Typography variant="body1" align="right">
-          v1.8-alpha
+          v1.8-beta
         </Typography>
       </Box>
     );
