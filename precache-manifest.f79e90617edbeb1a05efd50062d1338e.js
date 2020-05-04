899cefb37b5af4a57258eb3f066ecc95095f7154
elxris <elxris@users.noreply.github.com>
2020-05-04 00:28:55 +0000

    deploy: 96ed56708bd51f4246e1d4568c6f40760a5da6b8

---
+++ precache-manifest.f79e90617edbeb1a05efd50062d1338e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20560116215b2038e7b6",
+    "url": "./d1ebef836fe092e871d0.module.wasm"
+  },
+  {
+    "revision": "5fce03ee72ac58ae174d0f8f51d33023",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f32716cc22cc5647ed48",
+    "url": "./static/js/2.ec06a535.chunk.js"
+  },
+  {
+    "revision": "bc4b25c688496c9dd519b12c2b989a08",
+    "url": "./static/js/2.ec06a535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20560116215b2038e7b6",
+    "url": "./static/js/3.f450a0cb.chunk.js"
+  },
+  {
+    "revision": "2b107f6237bbed2b167b",
+    "url": "./static/js/main.bce6a2dc.chunk.js"
+  },
+  {
+    "revision": "bf4fffdf80c98ec87a30",
+    "url": "./static/js/runtime-main.2b20ddc1.js"
+  },
+  {
+    "revision": "98a0555d842e1fa47889f2bae6f00caa",
+    "url": "./static/media/ArialRoundedBold.98a0555d.woff"
+  },
+  {
+    "revision": "d2740563583d23ee04c832728f1042c5",
+    "url": "./static/media/FinkHeavyRegular.d2740563.woff"
+  },
+  {
+    "revision": "e8a8b570c16a7149c382e2f4d32433d7",
+    "url": "./static/media/FinkHeavyRegular.e8a8b570.woff2"
+  },
+  {
+    "revision": "d5f521c10f03b7f0f1f98411bd3f9a99",
+    "url": "./static/media/bells.d5f521c1.svg"
+  }
+]);
