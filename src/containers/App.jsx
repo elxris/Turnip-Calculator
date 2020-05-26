f33f3a877eadee5f8be007d4787641aba47aa83e
SHewitt95 <hewitt.sherman@gmail.com>
2020-05-26 00:47:26 -0400

    remove loose state

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -71,7 +71,7 @@ const App = () => {
                 });
               }}
             />
-            <Chart {...result} state={state} />
+            <Chart {...result} />
             <Table {...result} />
             <Footer />
           </Box>
