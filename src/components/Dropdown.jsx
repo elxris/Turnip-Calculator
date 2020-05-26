2d434b8159107a36f796875d84a52eec6e99140f
SHewitt95 <hewitt.sherman@gmail.com>
2020-05-26 19:09:23 -0400

    add FormControl, styles

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,24 +1,34 @@
 import React from "react";
-import { Select, MenuItem, InputLabel } from "@material-ui/core";
+import {
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  makeStyles,
+} from "@material-ui/core";
 import { string, arrayOf, shape, number, func } from "prop-types";
 
-const Dropdown = ({ menuItems, onChange, label, labelId, selectId }) => {
-  return (
-    <>
-      <InputLabel id={labelId}>{label}</InputLabel>
-      <Select labelId={labelId} id={selectId} onChange={onChange}>
-        <MenuItem value="">
-          <em>None</em>
+const useDropdownStyles = makeStyles({
+  formControl: {
+    width: "100%",
+  },
+});
+
+const Dropdown = ({ menuItems, onChange, label, labelId, selectId }) => (
+  <FormControl className={useDropdownStyles().formControl}>
+    <InputLabel id={labelId}>{label}</InputLabel>
+    <Select labelId={labelId} id={selectId} onChange={onChange}>
+      <MenuItem value="">
+        <em>Clear Selection</em>
+      </MenuItem>
+      {menuItems.map((item) => (
+        <MenuItem value={item.value} key={item.value}>
+          {item.text}
         </MenuItem>
-        {menuItems.map((item) => (
-          <MenuItem value={item.value} key={item.value}>
-            {item.text}
-          </MenuItem>
-        ))}
-      </Select>
-    </>
-  );
-};
+      ))}
+    </Select>
+  </FormControl>
+);
 
 Dropdown.propTypes = {
   menuItems: arrayOf(
