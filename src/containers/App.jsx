2e3fb774f50c8a7a1871898ed86fa7df5082c8a6
SHewitt95 <hewitt.sherman@gmail.com>
2020-05-25 23:19:31 -0400

    swap filters based on if rewind is enabled

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,9 +7,10 @@ import {
   theme,
   useShare,
   useCalculation,
+  useChartReducer,
 } from "../utils";
 import { Title, Filter, Footer } from "../containers";
-import { ShareDialog, Chart, Table } from "../components";
+import { ShareDialog, Table, Dropdown, StatefulChart } from "../components";
 
 const App = () => {
   useTitle();
@@ -21,7 +22,20 @@ const App = () => {
     shareFilters,
   } = useShare(filters);
 
-  const result = useCalculation({ filters });
+  const [state] = useChartReducer();
+  const { rewindEnabled, rewindFilters } = state;
+
+  // Get prediction/minMax values based on rewindFilters
+  // if rewindEnabled is true
+  let result = useCalculation({
+    filters: rewindEnabled ? rewindFilters : filters,
+  });
+  if (rewindEnabled) {
+    // Draw "Daily Price" based on the user's actual input.
+    // This allows the user to see their actual turnip prices graphed
+    // compared to past projections as they evolved.
+    result.filters = filters;
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -35,7 +49,8 @@ const App = () => {
               onChange={saveFilters}
               openShareDialog={openShareDialog}
             />
-            <Chart {...result} />
+            <Dropdown menuItems={[{ text: "Test" }]} />
+            <StatefulChart {...result} state={state} />
             <Table {...result} />
             <Footer />
           </Box>
