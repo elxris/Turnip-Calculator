efd2955de9759afe929272cfb48f9633df9c2e4a
GitHub Action <action@github.com>
2021-01-28 09:05:37 +0000

    Automatic Deploy

--- wasm/pkg/patterns_bg.js
+++ wasm/pkg/patterns_bg.js
@@ -108,8 +108,7 @@ function getStringFromWasm0(ptr, len) {
 */
 export function calculate(filters) {
     try {
-        const retptr = wasm.__wbindgen_export_0.value - 16;
-        wasm.__wbindgen_export_0.value = retptr;
+        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
         var ptr0 = passStringToWasm0(filters, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
         var len0 = WASM_VECTOR_LEN;
         wasm.calculate(retptr, ptr0, len0);
@@ -117,7 +116,7 @@ export function calculate(filters) {
         var r1 = getInt32Memory0()[retptr / 4 + 1];
         return getStringFromWasm0(r0, r1);
     } finally {
-        wasm.__wbindgen_export_0.value += 16;
+        wasm.__wbindgen_add_to_stack_pointer(16);
         wasm.__wbindgen_free(r0, r1);
     }
 }
